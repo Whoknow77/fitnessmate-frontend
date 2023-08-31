ee1139e549f8a5c63efe49f2496bcad5cd1ae740
Whoknow77 <Whoknow77@users.noreply.github.com>
2023-08-31 06:16:00 +0000

    Update from Frontend repo by Whoknow77

--- src/components/Navbar/StyledNavbar.js
+++ src/components/Navbar/StyledNavbar.js
@@ -3,34 +3,11 @@
 import styled from "styled-components";
 import theme from "./../../styles/theme";
 
-// < 스크롤이 있는 nav 아래 container 스타일 >
-
-// export const ScrollContainerWrapper = styled.div`
-//   width: 100%;
-//   display: flex;
-//   margin-top: 64px;
-//   margin-left: auto;
-//   margin-right: auto;
-//   padding-bottom: 200px;
-// `;
-
-// < 스크롤이 없는 nav 아래 container 스타일 >
-
-// export const NoneScrollContainerWrapper = styled.div`
-//   width: 100%;
-//   min-height: calc(100vh - 64px);
-//   display: flex;
-//   align-items: center;
-//   margin: 0 auto;
-//   justify-content: center;
-// `;
-
 export const NavbarContainer = styled.div`
   max-width: 1440px;
   width: 100%;
   height: 64px;
   padding: 10px 40px;
-  font-size: 15px;
   font-weight: 700;
   display: flex;
   position: sticky;
@@ -47,7 +24,7 @@ export const NavbarContainer = styled.div`
 
 export const NavLink = styled.div`
   display: flex;
-  gap: 43px;
+  gap: 24px;
 `;
 
 export const NavTextContainer = styled.div`
@@ -65,13 +42,12 @@ export const NavTextContainer = styled.div`
 `;
 
 export const NavButton = styled.button`
-  border-radius: 12px;
   color: ${theme.Black};
   display: flex;
-  padding: 10px 24px;
+  padding: 10px 14px;
   justify-content: center;
   align-items: center;
-  font-size: 15px;
+  font-size: 18px;
   font-weight: 700;
   line-height: 150%; /* 22.5px */
   letter-spacing: -0.3px;
@@ -83,24 +59,10 @@ export const NavButton = styled.button`
 export const NavLoginButton = styled(NavButton)`
   background: ${theme.BrandLight};
   color: ${theme.Brand};
+  padding: 10px 24px;
   display: block;
+  border-radius: 5px;
 `;
-
-export const NavProfileBox = styled.div`
-  transition: transform 0.2s ease-in-out 0s;
-  transform-origin: center top;
-  transform: scaleY(${({ isprofileBox }) => (isprofileBox ? "1" : "0")});
-  position: absolute;
-  top: 50px;
-  right: 15px;
-  display: flex;
-  align-items: center;
-  margin-top: 0;
-  margin-left: auto;
-  margin-right: auto;
-  justify-content: center;
-`;
-
 export const navbar = styled.div`
   .likes__wrapper {
     display: flex;
